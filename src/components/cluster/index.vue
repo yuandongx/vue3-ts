b52c7e9b875e9b5b1a6b615a1fcf0bfd669d85b4
yuandongx <786018072@qq.com>
2021-03-22 16:32:55 +0800

    .

--- src/components/cluster/index.vue
+++ src/components/cluster/index.vue
@@ -1,102 +1,46 @@
 <template>
-  <a-table :rowSelection="rowSelections" :data-source="data" :columns="columns" :rowKey="recoder => recoder.id" :loading="loading">
-    <template #filterDropdown="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }">
-      <div style="padding: 8px">
-        <a-input
-          :ref="c => (searchInput = c)"
-          :placeholder="`Search ${column.dataIndex}`"
-          :value="selectedKeys[0]"
-          style="width: 188px; margin-bottom: 8px; display: block;"
-          @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
-          @pressEnter="handleSearch(selectedKeys, confirm, column.dataIndex)"
-        />
-        <a-button
-          type="primary"
-          size="small"
-          style="width: 90px; margin-right: 8px"
-          @click="handleSearch(selectedKeys, confirm, column.dataIndex)"
+  <div>
+    <a-row>
+      <a-col :span="2">
+        <a-button type="link" @click="() => (addHostVisiable = true)"
+          >新建主机</a-button
         >
-          <template #icon><SearchOutlined /></template>
-          查找
-        </a-button>
-        <a-button size="small" style="width: 90px" @click="handleReset(clearFilters)">
-          重置
-        </a-button>
-      </div>
-    </template>
-    <template #filterIcon="filtered">
-      <search-outlined :style="{ color: filtered ? '#108ee9' : undefined }" />
-    </template>
-      <template #customRender="{ text, column }">
-      <span v-if="searchText && searchedColumn === column.dataIndex">
-        <template
-          v-for="(fragment, i) in text
-            .toString()
-            .split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))"
-        >
-          <mark v-if="fragment.toLowerCase() === searchText.toLowerCase()" class="highlight" :key="i">
-            {{ fragment }}
-          </mark>
-          <template v-else>{{ fragment }}</template>
-        </template>
-      </span>
-      <template v-else>
-        {{ text }}
-      </template>
-    </template>
-    <template #statusTags="{ text }">
-      <span>
-        <a-tag :color="tagColor(text)">{{ tagMsg(text) }}</a-tag>
-      </span>
-    </template>
-    <template #moreOperations="{ record }">
-
-      <Tooltip title="修改">
-      <EditTwoTone @click="modify(record)"/>
-      </Tooltip>
-      <divide type="vertical"/>
-      <Tooltip title="删除">
-      <DeleteTwoTone @click="deleteHosts(record)"/>
-      </Tooltip>
-      <divide type="vertical"/>
-      <Tooltip title="终端交互" @click="openConsole(record)">
-      <InteractionTwoTone/>
-      </Tooltip>
-    </template>
-  </a-table>
+      </a-col>
+      <a-col :span="2">
+        <a-button type="link" @click="showImportModal">批量导入主机</a-button>
+      </a-col>
+      <a-col :span="2">
+        <a-button type="link" @click="deleteHost">批量删除主机</a-button>
+      </a-col>
+    </a-row>
+    <a-divider></a-divider>
+    <!-- <display ref="refresh" @update="update"/> -->
+    <import-modal :showImport="showImport" />
+  </div>
 </template>
 
 <script lang="ts">
-import  { createVNode, defineComponent } from "vue";
-import { SearchOutlined, DeleteTwoTone, EditTwoTone, InteractionTwoTone, ExclamationCircleOutlined } from '@ant-design/icons-vue';
-import {Divider, Modal, Tooltip, Table, Button, Input, Tag, message} from "ant-design-vue";
-const data: [] = [];
-const colors = ["green", "default", "red", "warning"];
-const tagmsgs = ["验证成功", "未验证", "主机不可达", "认证失败"];
-export default defineComponent ({
+import { Button, Col, Divider, Row } from "ant-design-vue";
+import { defineComponent } from "@vue/runtime-core";
+import ImportModal from "./import.vue";
+export default defineComponent({
   components: {
-    [Tag.name]: Tag,
     [Button.name]: Button,
-    [Input.name]: Input,
-    [Table.name]: Table,
-    [Modal.name]: Modal,
-    SearchOutlined,
-    DeleteTwoTone,
-    EditTwoTone,
-    InteractionTwoTone,
-    divide: Divider,
-    Tooltip,
+    [Col.name]: Col,
+    [Row.name]: Row,
+    [Divider.name]: Divider,
+    "import-modal": ImportModal
   },
-  
+  data() {
+    return {
+      showImport: false
+    };
+  },
+  methods: {
+    showImportModal() {
+      this.showImport = true;
+    }
+  }
 });
 </script>
-<style scoped>
-.highlight {
-  background-color: rgb(255, 192, 105);
-  padding: 0px;
-}
-.menustyle{
-  border: 0em;
-  height: 10em;
-}
-</style>
+<style></style>
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
 import { createApp } from "vue";
-import MainApp from "./App.vue";
+import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import http from "./libs/http";
 import { AxiosInstance } from "axios";
-// import Axios from "axios";
 declare module "@vue/runtime-core" {
   export interface ComponentCustomProperties {
     $axios: AxiosInstance;
   }
 }
-const app = createApp(MainApp)
-  .use(store)
-  .use(router)
-  .mount("#app");
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount("#app");
 
 app.config.globalProperties.$http = http;
